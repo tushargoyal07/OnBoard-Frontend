c78f7b2ee4a75c99f7b9dd61283f1090ce9e3b00
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-01-28 01:32:40 +0530

    alignment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import darkMode from "../../public/assets/darkMode.svg";
 import circleFill from "../../public/assets/circleFill.svg";
+import priority from "../../public/assets/priority.svg";
 import axios from "axios";
 
 interface User {
@@ -49,35 +50,39 @@ export default function Card() {
         >
           <div className="flex items-center justify-between space-x-2 bg-gray-50 px-5 py-4 dark:bg-gray-700/50">
             <div>
-                <span className="flex text-sm text-gray-500">{ticket.id}</span>
+              <span className="flex text-sm text-gray-500">{ticket.id}</span>
             </div>
-           { data.users.map((user) => user.id === ticket.userId &&
-            <div key={user.id} className="-my-4">
-              <div className="inline-flex items-center justify-center space-x-2">
-                <div className="inline-flex size-8 items-center justify-center rounded-full bg-gray-100 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-500">
-                  {user.name[0]}
-                </div>
-              </div>
-              <div>
-                {user.available?(<div className="w-10 h-10 bg-blue-500 rounded-full"/>):(<div className="w-10 h-10 bg-red-500 rounded-full"/>)}
-              </div>
-            </div>)}
+            {data.users.map(
+              (user) =>
+                user.id === ticket.userId && (
+                  <div key={user.id} className="-my-4">
+                    <div className="inline-flex items-center justify-center space-x-2">
+                      <div className="inline-flex size-8 items-center justify-center rounded-full bg-gray-100 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-500">
+                        {user.name[0]}
+                      </div>
+                    </div>
+                    <div className="absolute right-10 -translate-y-2">
+                      {user.available ? (
+                        <div className="w-2 h-2 bg-blue-500 rounded-full" />
+                      ) : (
+                        <div className="w-2 h-2 bg-red-500 rounded-full" />
+                      )}
+                    </div>
+                  </div>
+                )
+            )}
           </div>
 
-          <div className="flex flex-row justify-center items-center grow">
-            <div className="flex">
-              <Image className="flex size-4 m-3" alt="" src={darkMode} />
-            </div>
-
-            <span className="flex grow p-3">
-             {ticket.title}
-            </span>
+          <div className="flex flex-row justify-center items-center grow px-5 ">
+            <Image className="flex size-4" alt="" src={darkMode} />
+            <span className="flex grow px-1 mx-3">{ticket.title}</span>
           </div>
 
-          <div className="flex  flex-row  px-2 py-4  dark:bg-gray-700/50 dark:text-gray-400">
-            <div className="flex  border rounded-md  px-1 items-center">
+          <div className="flex  flex-row px-5 py-4 dark:bg-gray-700/50 dark:text-gray-400">
+            <Image src={priority} alt="priority" className="" />
+            <div className="flex border rounded-md px-1 mx-3 items-center">
               <Image
-                className="flex size-3 mr-3 items fill-gray-600"
+                className="flex size-3 mr-1 items fill-gray-600"
                 alt=""
                 src={circleFill}
               />
--- src/components/Main/page.tsx
+++ src/components/Main/page.tsx
@@ -1,30 +1,22 @@
-'use client'
+"use client";
 
-import Nav from './nav'
-import Card from '../Card'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Ticket } from '@/types/ticket';
-
-
+import Nav from "./nav";
+import Card from "../Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Ticket } from "@/types/ticket";
 
 export default function Page() {
-
-
-    return (
-        <div className="bg-gray-100 min-h-screen">
-            <nav className="">
-                <div className="">
-                    <Nav />
-                </div>
-                <div className='grid grid-col'>
-                    <Card />
-                </div>
-
-            </nav>
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <nav className="">
+        <div className="">
+          <Nav />
         </div>
-    )
+        <div className="grid grid-col">
+          <Card />
+        </div>
+      </nav>
+    </div>
+  );
 }
-
-
-
