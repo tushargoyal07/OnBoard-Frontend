45f804fb95bc01d1bc8a67f145ce147cc1d83272
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-02-06 16:06:52 +0530

    added redux toolkit

--- src/components/Main/page.tsx
+++ src/components/Main/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-
 import Nav from "./nav";
 import Card from "../Card";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store.ts";
 
 export default function Page() {
+  const { grouping, ordering } = useSelector((state: RootState) => state.nav);
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <nav className="">
