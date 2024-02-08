6803d19454121ac071c5c6871942ad2b9ac55388
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-02-08 23:44:18 +0530

    made components of card and nav

--- src/components/Main/nav.tsx
+++ src/components/Main/nav.tsx
@@ -1,53 +1,51 @@
 import Image from "next/image";
 import plus from "../../../public/assets/plus.svg";
 import dots from "../../../public/assets/dots.svg";
-import Api from "../api";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/store.ts";
 
-interface Navbar {
-  navTitle: string;
+const statusData = [
+  { name: "Backlog" },
+  { name: "Todo" },
+  { name: "In progress" },
+  { name: "Done" },
+  { name: "Cancelled" },
+];
 
-  navCount: number;
-}
+const priorityData = [
+  { id: 0, name: "No Priority" },
+  { id: 1, name: "Low" },
+  { id: 2, name: "Medium" },
+  { id: 3, name: "High" },
+  { id: 4, name: "Urgent" },
+];
 
-export default function Nav() {
-  const { grouping, ordering } = useSelector((state: RootState) => state.nav);
-  const ticketCounts = {};
-  const data = Api();
-
+export default function Nav({ title, id }) {
   return (
     <>
-      {data.users.map((user) => (
-        <div className="flex flex-row justify-between py-4" key={user.id}>
-          {/* Left Section */}
-          <span>{grouping}</span>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-row">
-              <Image className="flex mx-2" alt="" src={user.priority} />
-              <span className="flex text-lg font-normal px-1">{user.name}</span>
-              {data.tickets.forEach((ticket) => {
-                if (ticket.userId === user.id) {
-                  ticketCounts[user.id] = ticketCounts[user.id]
-                    ? ticketCounts[user.id] + 1
-                    : 1;
-                }
-              })}
-              <span
-                key={user.id}
-                className="flex text-lg font-normal text-gray-500 px-2"
-              >
-                {ticketCounts[user.id]}
-              </span>
-            </div>
-            <div className="flex flex-row">
-              <Image className="flex m-1" alt="" src={plus} />
-              <p></p>
-              <Image className="flex m-1" alt="" src={dots} />
-            </div>
+      <div className="flex flex-row justify-between py-4">
+        <div className="flex items-center justify-between w-full">
+          <div className="flex flex-row">
+            <Image
+              className="flex mx-2"
+              alt=""
+              src={
+                grouping === "Status"
+                  ? `/assets/status/${title}.svg`
+                  : grouping === "User"
+                    ? "/assets/dots.svg"
+                    : `/assets/priority/${id}.svg`
+              }
+              width={16}
+              height={16}
+            />
+            <span className="flex text-lg font-normal px-1">{title}</span>
+          </div>
+          <div className="flex flex-row">
+            <Image className="flex m-1" alt="" src={plus} />
+            <p></p>
+            <Image className="flex m-1" alt="" src={dots} />
           </div>
         </div>
-      ))}
+      </div>
     </>
   );
 }
--- src/components/Main/page.tsx
+++ src/components/Main/page.tsx
@@ -3,16 +3,60 @@ import Nav from "./nav";
 import Card from "../Card";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store.ts";
+import { useEffect, useState } from "react";
+import Api from "../api";
+
+const statusData = [
+  { id: 1, name: "Backlog" },
+  { id: 2, name: "Todo" },
+  { id: 3, name: "In progress" },
+  { id: 4, name: "Done" },
+  { id: 5, name: "Cancelled" },
+];
+
+const priorityData = [
+  { id: 0, name: "No Priority" },
+  { id: 1, name: "Low" },
+  { id: 2, name: "Medium" },
+  { id: 3, name: "High" },
+  { id: 4, name: "Urgent" },
+];
 
 export default function Page() {
+  const [filterType, setFilterType] = useState<any[]>(statusData);
   const { grouping, ordering } = useSelector((state: RootState) => state.nav);
+  const data = Api();
 
+  useEffect(() => {
+    if (grouping === "Status") {
+      setFilterType(statusData);
+    } else if (grouping === "Priority") {
+      setFilterType(priorityData);
+    } else if (grouping === "User") {
+      setFilterType(data.users);
+    }
+  }, [grouping, ordering, data.users]);
+
+  /*
+  const handleCards = () => {
+    if (grouping === 'User') {
+      return data.tickets.userId === data.users.id
+    }else if(grouping === 'Priority')
+  };
+  */
   return (
     <div className="bg-gray-100 min-h-screen">
       <nav className="">
         <div className="grid grid-cols-5">
-          <Nav />
-          <Card />
+          {filterType.map((filter) => (
+            <div className="" key={filter.id}>
+              <Nav title={filter.name} id={filter.id} />
+              {/* {data.tickets.map(
+                handleCards
+              )} */}
+              <Card />
+            </div>
+          ))}
         </div>
       </nav>
     </div>
