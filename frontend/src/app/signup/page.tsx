47f87083a2273733090e0d8bef3ceb5c9751e016
Lovish Bansal <112380541+lovishbansal00@users.noreply.github.com>
2024-02-22 10:55:59 +0000

    added signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-export const Signup = () => {
+export default function Signup(){
   return (
     <>
-      <div className="self-center">
-        <h1>Sign Up</h1>
-        <form action="">
-          <input type="text" id="firstName" placeholder="Frist Name" />
-          <input type="text" id="lastName" />
+      <div className="flex flex-col items-center justify-content bg-blue-100 mx-auto md:max-w-4xl mx-w-2xl">
+        <h1 className="text-2xl font-semibold ">Sign Up</h1>
+        <form className="flex flex-col" action="">
+          <label >First Name</label>
+          <input type="text" id="firstName" placeholder="Enter Your First Name" />
+          <label >Last Name</label>
+          <input type="text" id="firstName" placeholder="Enter your last name" />
+          <label >Email Address</label>
+          <input type="email" id="email" placeholder="Enter your email adderss" />
         </form>
       </div>
     </>
