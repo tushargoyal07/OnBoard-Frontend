7f03996b49487e5b717ac1314319581b6799bb33
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-02-24 11:20:11 +0530

    added jdbc dependency

--- onboard-backend/src/main/java/com/onboard/onboardbackend/Test.java
+++ onboard-backend/src/main/java/com/onboard/onboardbackend/Test.java
@@ -1,12 +1,4 @@
 package com.onboard.onboardbackend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class Test {
-    @GetMapping("/test")
-    public String test(){
-        return "first rest api in java springboot";
-    }
 }
