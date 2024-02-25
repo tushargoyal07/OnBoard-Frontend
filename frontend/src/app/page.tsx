f9fa57449cd91a930532592fb66b3def09dee4b1
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-02-25 23:22:49 +0530

    fix:repo structure

--- frontend/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import Page from "@/components/Main/page";
-
-export default function landingpage() {
-  return <Page />;
-}
--- frontend/src/types/ticket.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Ticket {
-  id: string;
-  title: string;
-  tag: string[];
-  userId: string;
-  status: string;
-  priority: number;
-}
