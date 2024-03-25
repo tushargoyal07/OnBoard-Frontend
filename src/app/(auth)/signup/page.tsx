3e609d95ed6d58ad1e4df4bc8e1f5b13134325f1
Tushar <goyalt796@gmail.com>
2024-03-26 00:53:26 +0530

    signup ui

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -35,65 +35,75 @@ export default function Signup() {
       );
       if (res.ok) {
         const data = await res.text();
+        router.push("/login");
         console.log(data);
       } else {
         console.log("Sign up failed");
       }
-      router.push("/login");
+
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div>
-      <div className="flex flex-col items-center justify-content bg-blue-100 mx-auto md:max-w-4xl mx-w-2xl">
-        <h1 className="text-2xl font-semibold ">Sign Up</h1>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <label>First Name</label>
-          <input
-            type="text"
-            id="firstname"
-            className="w-full p-2 border border-gray-300"
-            placeholder="Enter Your First Name"
-            onChange={handleChange}
-          />
-          <label>Last Name</label>
-          <input
-            type="text"
-            id="lastname"
-            placeholder="Enter your last name"
-            className="w-full p-2 border border-gray-300"
-            onChange={handleChange}
-          />
-          <label>Email Address</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email adderss"
-            className="w-full p-2 border border-gray-300"
-            onChange={handleChange}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Create your password"
-            className="w-full p-2 border border-gray-300"
-            onChange={handleChange}
-          />
-          <button
-            type="submit"
-            className="border border-green-300 text-black bg-white mt-3 border-rounded"
-          >
-            Sign Up
-          </button>
-        </form>
-      </div>
-      <div className="flex gap-4 self-center">
-        <p>Alerady have an account ?</p>
-        <button onClick={() => router.push("/login")}>Sign In</button>
-      </div>
+    <div className="min-h-screen bg-[#f5f0e1] p-5">
+        <div className="card border-2 py-5 rounded-lg bg-white flex flex-col items-center justify-content mx-auto md:max-w-4xl mx-w-2xl">
+          <h1 className="card-header text-2xl font-bold my-3">Sign Up</h1>
+          <form className="card-body gap-5 my-5 flex flex-col" onSubmit={handleSubmit}>
+            <div >
+              <label className="">First Name</label>
+              <input
+                type="text"
+                id="firstname"
+                className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+                placeholder="Enter First Name"
+                onChange={handleChange}
+              />
+            </div>
+            <div >
+              <label>Last Name</label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="Enter Last Name"
+                className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+                onChange={handleChange}
+              />
+            </div>
+            <div >
+              <label>Email Address</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter Email Adderss"
+                className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+                onChange={handleChange}
+              />
+            </div>
+            <div >
+              <label>Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Create Password"
+                className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className="my-3 bg-green-500 px-3 py-1 rounded text-white font-semibold active:bg-blue-300 focus:ring-2 ring-blue-700 transition duration-300 hover:scale-110 hover:ease-in-outn-75"
+            >
+              Sign Up
+            </button>
+          </form>
+          <div className="card-footer flex-col text-center justify-center">
+            <p className=" my-5 ">Alerady have an account ?</p>
+            <button className="bg-red-500 px-3 py-1 rounded text-white font-semibold active:bg-blue-300 focus:ring-2 ring-blue-700 transition duration-300 hover:scale-110  hover:ease-in-out" onClick={() => router.push("/login")}>Sign In</button>
+          </div>
+        </div>
+      
     </div>
   );
 }
