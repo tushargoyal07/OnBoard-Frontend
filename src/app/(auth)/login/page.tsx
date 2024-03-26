394c9afb0858610aa2fad6f2f67cc416cb550ba6
Mudita Aggarwal <muditaaggarwal8@gmail.com>
2024-03-26 19:22:06 +0530

    login ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -51,38 +51,44 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="mx-auto text-center">
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="">
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="w-auto border rounded border-blue-500"
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="w-auto border rounded border-blue-500"
-          />
-        </div>
-        <button
-          type="submit"
-          className="border rounded border-black mt-3 w-auto text-blue-700 font-bold text-2xl"
-        >
-          Login
-        </button>
-      </form>
+    <div className="min-h-screen bg-[#f5f0e1] p-5">
+      <div className="card border-2 py-5 rounded-lg bg-white flex flex-col items-center justify-content mx-auto md:max-w-4xl mx-w-2xl">
+        <h1 className="card-header text-2xl font-bold my-3">Sign In</h1>
+        <form className="card-body gap-5 my-5 flex flex-col" onSubmit={handleSubmit}>
+          <div className="">
+            <label>Email Address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter Email"
+              className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter Password"
+              className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="my-3 bg-green-500 px-3 py-1 rounded text-white font-semibold active:bg-blue-300 focus:ring-2 ring-blue-700 transition duration-300 hover:scale-110 hover:ease-in-outn-75"
+          >
+            Login
+          </button>
+        </form>
 
-      <div className="">
-        <p>Don&apos;t have an account ?</p>
-        <button onClick={() => router.push("/signup")}>Sign Up</button>
+        <div className="card-footer flex-col text-center justify-center">
+          <p className="my-5">Don&apos;t have an account ?</p>
+          <button className="bg-red-500 px-3 py-1 rounded text-white font-semibold active:bg-blue-300 focus:ring-2 ring-blue-700 transition duration-300 hover:scale-110  hover:ease-in-out" onClick={() => router.push("/signup")}>Sign Up</button>
+        </div>
       </div>
     </div>
   );
